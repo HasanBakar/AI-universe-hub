2d301fde26ee5ad5fae8df8e51bce041a9988657
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-03-02 22:59:54 +0600

    setted name, date, and arrow button re-positioning

--- js/index.js
+++ js/index.js
@@ -27,10 +27,14 @@ const showCard = data =>{
                         <li>3. ${features[2]} </li>
                     </ol>
                     <hr>
-                    <h3 class="text-2xl font-semibold"> ${name}</h3>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                    <div class="flex justify-between items-center">
+                        <div>
+                            <h3 class="text-2xl font-semibold"> ${name}</h3>
+                            <p><i class="fa-solid fa-calendar-days text-primary"></i> ${published_in}</p>
+                        </div>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></button>
+                        </div>
                     </div>
                 </div>
             </div>
